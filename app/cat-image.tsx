477ac5f2c09805388f27cdce73ad458198ba342b
岩本和樹 <kazuki@iwamotokazukinoMacBook-Air.local>
2025-06-12 15:59:50 +0900

    コミットメッセージ

--- app/cat-image.tsx
+++ app/cat-image.tsx
@@ -1,6 +1,7 @@
 "use client"; // コンポーネントの引数を定義する
 import { useState } from "react";
 import { fetchImage } from "./fetch-image";
+import styles from "./page.module.css"; 
 type CatImageProps = {
   url: string;
 };
@@ -16,22 +17,14 @@ export function CatImage({ url }: CatImageProps) {
 
   return (
     
-    <div>
-      <button
-      onClick={refreshImage}
-      style={{
-        padding: '10px 20px',
-        border: '2px solid #8BC34A', // 黄緑色枠線
-        borderRadius: '5px', // 角を丸くする
-        backgroundColor: '#8BC34A', // 背景色を黄緑色に
-        color: 'white', // テキスト色を白に
-        cursor: 'pointer', // カーソルをポインターに
-        fontSize: '16px', // フォントサイズ
-        fontWeight: 'bold', // フォントを太く
-        marginTop: '20px', // 上部に余白
-      }}
-    >他のにゃんこも見る</button>
-      {imageUrl && <img src={imageUrl} />}
+    
+    <div className={styles.page}>
+    <button onClick={refreshImage} className={styles.button}>
+      他のにゃんこも見る
+    </button>
+    <div className={styles.frame}>
+      {imageUrl && <img src={imageUrl} className={styles.img} />}
     </div>
+  </div>
   );
 }
--- app/fetch-image.ts
+++ app/fetch-image.ts
@@ -1,6 +1,13 @@
-export async function fetchImage() {
-  const res = await fetch("https://api.thecatapi.com/v1/images/search");
+"use server";
+import { CAT_API_KEY } from "./env";
+type Image = {
+  url: string;
+};
+export async function fetchImage(): Promise<Image> {
+  const res = await fetch("https://api.thecatapi.com/v1/images/search", {
+    headers: { "x-api-key": CAT_API_KEY }, // 追加
+  });
   const images = await res.json();
   console.log("fetchImage: 画像情報を取得しました", images);
-  return images[0]; // 画像情報の配列から最初の要素を返す
+  return images[0];
 }
